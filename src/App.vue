9c6269f822c2ad903de9491c797b93f42a3ff661
cboncales <clarkboncales115@gmail.com>
2025-01-25 22:44:27 +0800

    added navdrawer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import AppBar from './components/Appbar.vue'
+import NavDrawer from './components/NavDrawer.vue'
 </script>
 
 <template>
   <v-app>
     <AppBar />
+    <NavDrawer />
   </v-app>
 </template>
 
---
+++ src/components/NavDrawer.vue
@@ -0,0 +1,30 @@
+<script setup></script>
+<template>
+  <div>
+    <v-navigation-drawer app expand-on-hover rail>
+      <v-list>
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
+          subtitle="johndoe@gmailcom"
+          title="John Doe"
+        ></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+        <v-list-item
+          prepend-icon="mdi-account-multiple"
+          title="Shared with me"
+          value="shared"
+        ></v-list-item>
+        <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main style="height: 250px"></v-main>
+  </div>
+</template>
+
+<style scoped></style>
