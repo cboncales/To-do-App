c8caa52660246353cb8c43991eb760cc6cb0a3f8
cboncales <clarkboncales115@gmail.com>
2025-01-26 15:49:24 +0800

    added addTask functionality

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,12 +1,33 @@
 <script setup>
 import { ref } from 'vue'
 import TaskModal from './layout/TaskModal.vue'
-const listContainer = document.getElementById('list-container')
-const inputBox = document.getElementById('input-box')
 
+// Reactive tasks list
+const tasks = ref([])
 const isVisible = ref(false)
-const addTask = ref()
+
+// Mark task as done/undone
+const doneTask = (id) => {
+  const task = tasks.value.find((task) => task.id === id)
+  if (task) task.done = !task.done
+}
+
+// Delete task
+const deleteTask = (id) => {
+  tasks.value = tasks.value.filter((task) => task.id !== id)
+}
+
+// Edit task
+const editTask = (task) => {
+  task.editing = !task.editing
+}
+
+// Add a new task from TaskModal
+const addTask = (newTask) => {
+  tasks.value.push(newTask)
+}
 </script>
+
 <template>
   <v-main style="height: 250px" app>
     <v-container>
@@ -21,42 +42,27 @@ const addTask = ref()
                 size="large"
                 icon="mdi-plus"
                 color="orange-darken-2"
-                @click="
-                  () => {
-                    isVisible = true
-                  }
-                "
-              ></v-btn>
-            </div>
-            <v-card-text>
-              <v-list>
-                <v-list-item>
-                  <v-icon>mdi-checkbox-blank-outline</v-icon>
-                  <v-list-item-title>Take a Bath</v-list-item-title>
-                  <v-list-item-subtitle>maligo ko</v-list-item-subtitle>
-                </v-list-item>
-              </v-list>
-            </v-card-text>
-
-            <v-divider></v-divider>
-
-            <div class="d-flex justify-space-between">
-              <v-card-title class="my-2">Tomorrow's Tasks</v-card-title>
-              <v-btn
-                class="mx-5 my-1"
-                density="compact"
-                size="large"
-                icon="mdi-plus"
-                color="orange-darken-2"
                 @click="isVisible = true"
               ></v-btn>
             </div>
             <v-card-text>
               <v-list>
-                <v-list-item>
-                  <v-icon>mdi-checkbox-blank-outline</v-icon>
-                  <v-list-item-title>Take a Bath</v-list-item-title>
-                  <v-list-item-subtitle>maligo ko</v-list-item-subtitle>
+                <v-list-item v-for="task in tasks" :key="task.id">
+                  <v-icon @click="doneTask(task.id)">
+                    {{ task.done ? 'mdi-checkbox-marked' : 'mdi-checkbox-blank-outline' }}
+                  </v-icon>
+                  <v-list-item-title>{{ task.title }}</v-list-item-title>
+                  <v-list-item-subtitle>{{ task.time }}</v-list-item-subtitle>
+                  <v-list-item-action>
+                    <v-btn @click.stop="deleteTask(task.id)" icon>
+                      <v-icon color="blue-grey darken-1">mdi-delete</v-icon>
+                    </v-btn>
+                    <v-btn @click.stop="editTask(task)" icon>
+                      <v-icon color="blue-grey darken-1">
+                        {{ task.editing ? 'mdi-check' : 'mdi-pencil' }}
+                      </v-icon>
+                    </v-btn>
+                  </v-list-item-action>
                 </v-list-item>
               </v-list>
             </v-card-text>
@@ -64,7 +70,11 @@ const addTask = ref()
         </v-col>
       </v-row>
     </v-container>
+    <!-- TaskModal -->
+    <TaskModal
+      :isAddTaskVisible="isVisible"
+      @update:isAddTaskVisible="isVisible = $event"
+      @add-task="addTask"
+    ></TaskModal>
   </v-main>
-  <TaskModal v-model:isAddTaskVisible="isVisible"></TaskModal>
 </template>
-<style scoped></style>
